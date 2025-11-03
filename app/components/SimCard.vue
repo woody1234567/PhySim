3c7c569493e24a0227fcd87363628d64fbf19ca2
woody <johannes8873@gmail.com>
2025-11-03 16:38:53 +0800

    feat: Adding a new SimCard component

---
+++ app/components/SimCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="card bg-base-100 w-96 shadow-sm">
+      <figure class="px-10 pt-10">
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+          alt="Shoes"
+          class="rounded-xl"
+        />
+      </figure>
+      <div class="card-body items-center text-center">
+        <h2 class="card-title">Card Title</h2>
+        <p>
+          A card component has a figure, a body part, and inside body there are
+          title and actions parts
+        </p>
+        <div class="card-actions">
+          <button class="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,8 +2,18 @@
   <div>
     <hero></hero>
   </div>
+  <div class="flex justify-center mt-10">
+    <div class="grid grid-cols-3 gap-5">
+      <sim-card />
+      <sim-card />
+      <sim-card />
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import SimCard from "~/components/SimCard.vue";
+import Hero from "~/components/Hero.vue";
+</script>
 
 <style scoped></style>
